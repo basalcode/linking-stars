64073ca0af5362b60d36a4bbc0e7e5bc819870e2
basalcode <basalcode@gmail.com>
2022-01-17 23:53:30 +0900

    Create clock and set style

--- src/pages/clock.tsx
+++ src/pages/clock.tsx
@@ -1,13 +1,22 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react';
 
 import Layout from '../components/common/Layout';
 
 const Clock: NextPage = () => {
+    const [time, setTime] = useState(Date.now());
+
+    useEffect(() => {
+        setTimeout(() => {
+            setTime(Date.now());
+        }, 1000);
+    }, [time]);
+
     return (
         <Layout>
-            Clock Page
+            {time}
         </Layout>
     );
 }
 
-export default Clock;
+export default Clock; 
