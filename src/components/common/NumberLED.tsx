fe9813bcc52ec8db53b094d997a9ef36fa0d6de7
basalcode <basalcode@gmail.com>
2022-01-21 23:51:27 +0900

    Create led number data

--- src/components/common/NumberLED.tsx
+++ src/components/common/NumberLED.tsx
@@ -1,8 +1,30 @@
 import type { NextPage } from 'next'
 
+import style from './NumberLED.module.scss';
+
 const NumberLED: NextPage = () => {
+    const ledLight: object = {
+        one: [false, false, true, false, false, true, false],
+        two: [false, false, true, false, false, true, false],
+        three: [false, false, true, false, false, true, false],
+        four: [false, false, true, false, false, true, false],
+        five: [false, false, true, false, false, true, false],
+        six: [false, false, true, false, false, true, false],
+        seven: [false, false, true, false, false, true, false],
+        eight: [false, false, true, false, false, true, false],
+        nine: [false, false, true, false, false, true, false]
+    }
+
     return (
-        <div></div>
+        <div className={style.numberLED}>
+            <div className={`${style.ledLine} ${style["ledLine--on"]}`}></div>
+            <div className={`${style.ledLine} ${style["ledLine--on"]}`}></div>
+            <div className={`${style.ledLine} ${style["ledLine--on"]}`}></div>
+            <div className={`${style.ledLine} ${style["ledLine--on"]}`}></div>
+            <div className={`${style.ledLine} ${style["ledLine--on"]}`}></div>
+            <div className={`${style.ledLine} ${style["ledLine--on"]}`}></div>
+            <div className={`${style.ledLine} ${style["ledLine--on"]}`}></div>
+        </div>
     );
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,5 @@
 import Document, { DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript } from 'next/document';
 
-import style from '..\styles\document.module.scss';
-
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
         const initialProps = await Document.getInitialProps(ctx);
