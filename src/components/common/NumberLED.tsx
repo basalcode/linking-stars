4a701e9f8d69e2f8367998b9ea26e206c79ae1ef
basalcode <basalcode@gmail.com>
2022-01-25 23:54:11 +0900

    Create position of led NumberLED

--- src/components/common/NumberLED.tsx
+++ src/components/common/NumberLED.tsx
@@ -4,10 +4,10 @@ import { useMemo } from 'react';
 import style from './NumberLED.module.scss';
 
 const NumberLED: NextPage = () => {
-    const number = 2;
+    const number = 3;
 
     const ledLight = [
-        [true, true, true, true, false, true, true],
+        [false, true, false, false, false, true, false],
         [false, false, true, false, false, true, false],
         [false, true, true, false, true, true, true],
         [false, true, true, false, true, true, true],
@@ -20,14 +20,29 @@ const NumberLED: NextPage = () => {
         [true, true, true, false, true, true, false],
     ];
 
+    const ledPosition = [
+        { x: 0, y: 0, width: 10, height: 2 },
+        { x: 0, y: 0, width: 2, height: 10 },
+        { x: 10, y: 0, width: 2, height: 10 },
+        { x: 0, y: 10, width: 10, height: 2 },
+        { x: 0, y: 10, width: 2, height: 10 },
+        { x: 10, y: 10, width: 2, height: 10 },
+        { x: 0, y: 20, width: 10, height: 2 }
+    ]
+
     const currentLedLight = ledLight[number - 1];
 
     const ledLines = useMemo(() => {
         return currentLedLight.map((isOn, index) => {
-
             return (
                 <div
                     key={index}
+                    style={{
+                        top: ledPosition[index].y + 50,
+                        left: ledPosition[index].x + 50,
+                        width: ledPosition[index].width,
+                        height: ledPosition[index].height
+                    }}
                     className={`${style.ledLine} ${isOn && style["ledLine--on"]}`}
                 ></div>
             );
@@ -37,6 +52,7 @@ const NumberLED: NextPage = () => {
     return (
         <div className={style.numberLED}>
             {ledLines}
+
         </div>
     );
 }
