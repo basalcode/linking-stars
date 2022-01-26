3bd85944abd75e6dd1ee8648956bb651f3e16370
basalcode <basalcode@gmail.com>
2022-01-27 00:08:17 +0900

    Update led data correctly

--- src/components/common/NumberLED.tsx
+++ src/components/common/NumberLED.tsx
@@ -4,14 +4,13 @@ import { useMemo } from 'react';
 import style from './NumberLED.module.scss';
 
 const NumberLED: NextPage = () => {
-    const number = 3;
+    const number = 9;
 
     const ledLight = [
-        [false, true, false, false, false, true, false],
+        [true, true, true, true, false, true, true],
         [false, false, true, false, false, true, false],
+        [false, true, true, true, true, false, true],
         [false, true, true, false, true, true, true],
-        [false, true, true, false, true, true, true],
-        [false, false, true, false, false, true, false],
         [true, false, true, false, true, true, false],
         [true, true, false, false, true, true, true],
         [true, false, false, true, true, true, true],
@@ -21,16 +20,16 @@ const NumberLED: NextPage = () => {
     ];
 
     const ledPosition = [
-        { x: 0, y: 0, width: 10, height: 2 },
         { x: 0, y: 0, width: 2, height: 10 },
+        { x: 0, y: 0, width: 10, height: 2 },
         { x: 10, y: 0, width: 2, height: 10 },
-        { x: 0, y: 10, width: 10, height: 2 },
         { x: 0, y: 10, width: 2, height: 10 },
+        { x: 0, y: 10, width: 10, height: 2 },
         { x: 10, y: 10, width: 2, height: 10 },
         { x: 0, y: 20, width: 10, height: 2 }
     ]
 
-    const currentLedLight = ledLight[number - 1];
+    const currentLedLight = ledLight[number];
 
     const ledLines = useMemo(() => {
         return currentLedLight.map((isOn, index) => {
@@ -38,13 +37,12 @@ const NumberLED: NextPage = () => {
                 <div
                     key={index}
                     style={{
-                        top: ledPosition[index].y + 50,
-                        left: ledPosition[index].x + 50,
+                        top: ledPosition[index].y,
+                        left: ledPosition[index].x,
                         width: ledPosition[index].width,
                         height: ledPosition[index].height
                     }}
                     className={`${style.ledLine} ${isOn && style["ledLine--on"]}`}
-
                 ></div>
             );
         });
@@ -53,7 +51,6 @@ const NumberLED: NextPage = () => {
     return (
         <div className={style.numberLED}>
             {ledLines}
-
         </div>
     );
 }
