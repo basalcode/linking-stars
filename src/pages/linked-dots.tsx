ba67ebf20b21efba2e2d8c3abdb0e46cd1036fdf
basalcode <basalcode@gmail.com>
2022-02-04 23:55:34 +0900

    Try to find point draw feature error

--- src/pages/linked-dots.tsx
+++ src/pages/linked-dots.tsx
@@ -1,42 +1,37 @@
 import type { NextPage } from 'next'
-import { useEffect, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 
 import Layout from '../components/common/Layout';
 
 import style from './linked-dots.module.scss';
 
 const LinkedDots: NextPage = () => {
-    const canvasRef = useRef(null);
 
     useEffect(() => {
-        if (!canvasRef?.current) return;
-
-        const canvasElement = canvasRef.current;
+        const canvasElement = document.getElementById("linkedDotsCanvas");
         const context = canvasElement.getContext('2d');
 
         const pointAmount = 10;
-
         const width = canvasElement.clientWidth;
         const height = canvasElement.clientHeight;
+        console.log("width", width);
+        console.log("height", height);
 
         for (let i = 0; i < pointAmount; i++) {
             const randomX = Math.floor(Math.random() * width);
             const randomY = Math.floor(Math.random() * height);
-            const radius = 1;
 
-            context.beginPath();
-            context.arc(randomX, randomY, radius, 0, 2 * Math.PI, true);
-            context.fill();
-            context.stroke();
+            console.log("randomX", randomX, "randomY", randomY);
 
+            context.rect(randomX, randomY, 1, 1);
         }
-    }, [canvasRef]);
+        context.fill();
+    }, []);
 
     return (
         <Layout>
             <canvas
-                id="canvas"
-                ref={canvasRef}
+                id="linkedDotsCanvas"
                 className={style.canvas}
             ></canvas>
         </Layout>
