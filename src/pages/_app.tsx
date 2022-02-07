360d1c999fa9245e69db49cadb5fad99aae977b7
basalcode <basalcode@gmail.com>
2022-02-07 23:52:12 +0900

    Update folders

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,10 @@
 import type { AppProps } from 'next/app'
 
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-
-import { store, persistor } from 'store/config';
-
 import 'styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Component {...pageProps} />
-      </PersistGate>
-    </Provider>
+    <Component {...pageProps} />
   );
 }
 
